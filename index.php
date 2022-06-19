a20127c6945b850378fe4b247ceeedb5ba100023
Waellersom <waellersom@gmail.com>
2022-06-19 12:18:25 -0300

    Adiciona a navbar

--- index.php
+++ index.php
@@ -7,13 +7,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mac Lojas</title>
     <link rel="stylesheet" href="./libs/bootstrap-5.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/src/css/styles.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./src/css/style.css">
 </head>
 
 <body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="./src/assets/img/logo.png" alt="logo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Quem Somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Produtos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Localização</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div id="app"></div>
+    </main>
 
-    <script src="/libs/bootstrap-5.2.0/js/bootstrap.min.js"></script>
+    <script src="./libs/bootstrap-5.2.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
